415bb5da0c0e1308636c82a5f1c1e83d83417937
vipnat <tuanit198x@gmail.com>
2016-02-27 20:01:42 +0700

    Commit Đổi Ten File

--- QL_BanDayNit/DoiTenFile.cs
+++ QL_BanDayNit/DoiTenFile.cs
@@ -14,7 +14,9 @@ namespace QL_BanDayNit
         public frmDoiTenFile()
         {
             InitializeComponent();
-        }      
+        }
+
+        string[] files = null;
 
         private void frmDoiTenFile_Load(object sender, EventArgs e)
         {
@@ -23,67 +25,119 @@ namespace QL_BanDayNit
 
         private void btnOk_Click(object sender, EventArgs e)
         {
-            
-            FolderBrowserDialog fl = new FolderBrowserDialog();
-            fl.SelectedPath = "c:\\";//đường dẫn mặc định thư mục lúc mở ra
-            fl.ShowNewFolderButton = true;
-            if (fl.ShowDialog() == DialogResult.OK)//gọi thư mục
-            {
-                ShowFile(fl.SelectedPath);//duyệt thư mục
-            }
+            FolderBrowserDialog fld = new FolderBrowserDialog();
+            fld.RootFolder = Environment.SpecialFolder.MyComputer;
+            fld.ShowDialog();
+            txtDirectory.Text = fld.SelectedPath;
+            files = System.IO.Directory.GetFiles(txtDirectory.Text.Trim());
+            txtTotalFile.Text = files.Length.ToString();
         }
 
-        //Lấy Danh Sách File Trong Thư Mục Và Đổi Tên
-        //System.IO.File.Move((string)oldFileName, (string)newFileName) // đổi tên cho file.
-        //System.IO.Directory.Move((string)oldFolderName, (string)newFolderName) // đổi tên cho thư mục.
-
-        public void ShowFile(string path)
+        private void btnDoiTen_Click(object sender, EventArgs e)
         {
-            if (File.Exists(path))
+            // Lấy tất cả các tập tin trong thư mục truyền vào
+            if (files == null)
+                return;
+            string originalFile = "", renamedFile = "";
+            int countSuccess = 0, startedNumber = Convert.ToInt16(txtFirstNumber.Text);
+
+            for (int i = 0; i < files.Length; i++)
             {
-                ProcessFile(path, "");//nếu file không tồn tại
+                originalFile = files[i];
+
+                // cắt chuỗi bởi dấu '.' -> lấy phần mở rộng của tập tin, ví dụ .jpg, .doc, ...
+                string[] fileParts = originalFile.Split(new char[] { '.' });
+
+                // Tạo Số Đồng Bộ
+                string strNumber = "";
+                if(files.Length < 100)
+                    strNumber = startedNumber++.ToString("00");
+                if(files.Length >= 100)
+                    strNumber = startedNumber++.ToString("000");
+
+                // Tạo đường dẫn và tập tin đích
+                renamedFile = txtDirectory.Text.Trim() + @"\\" + txtFileName.Text.Trim() + strNumber + "." + fileParts[fileParts.Length - 1];
+
+                // Kiểm tra tập tin nguồn & đích
+                // Nếu tập tin nguồn không tồn tại hoặc tập tin đích đã tồn tại rồi thì bỏ qua, không đổi tên
+                if (!System.IO.File.Exists(originalFile) && System.IO.File.Exists(renamedFile))
+                    continue;
+
+                // Đổi tên sau khi kiểm tra hợp lệ
+                System.IO.File.Move(originalFile, renamedFile);
+                countSuccess++; // biến đếm tăng lên 1 khi đổi tên thành công
             }
-            else if (Directory.Exists(path))
-            {
-                ProcessDirectory(path); // nếu file tồn tại
-            }
-            else
-            {
-                Console.WriteLine("{0} : Không đọc được file.", path);
-            }
+            MessageBox.Show("Đổi Tên Thành Công " + countSuccess + " File.");
+            files = null;
+            txtDirectory.Clear();
+            txtTotalFile.Text = "0";
+            txtFileName.Clear();
+            txtFirstNumber.Text = "0";
         }
 
-        public void ProcessDirectory(string pathfile)
-        {
-            string[] fileList = Directory.GetFiles(pathfile);//lay danh sách file cho vao mảng
-            string strFileName = "";
-            //duyet mang file trong thư mục
+        //private void btnOk_Click(object sender, EventArgs e)
+        //{
 
-            foreach (string fileName in fileList)
-            {
-                strFileName = "";
-                strFileName = Path.GetFileName(fileName).Trim();
-                ProcessFile(fileName, strFileName);
-            }
+        //    FolderBrowserDialog fl = new FolderBrowserDialog();
+        //    fl.SelectedPath = "D:\\";//đường dẫn mặc định thư mục lúc mở ra
+        //    fl.ShowNewFolderButton = true;
+        //    if (fl.ShowDialog() == DialogResult.OK)//gọi thư mục
+        //    {
+        //        ShowFile(fl.SelectedPath);//duyệt thư mục
+        //    }
+        //}
 
-            string[] directorylist = Directory.GetDirectories(pathfile);//lấy danh sách target file cho vào mảng
+        ////Lấy Danh Sách File Trong Thư Mục Và Đổi Tên
+        ////System.IO.File.Move((string)oldFileName, (string)newFileName) // đổi tên cho file.
+        ////System.IO.Directory.Move((string)oldFolderName, (string)newFolderName) // đổi tên cho thư mục.
 
-            //duyệt mảng target
-            foreach (string directory in directorylist)
-            {
-                ProcessDirectory(directory);
-            }
-        }
+        //public void ShowFile(string path)
+        //{
+        //    if (File.Exists(path))
+        //    {
+        //        ProcessFile(path, "");//nếu file không tồn tại
+        //    }
+        //    else if (Directory.Exists(path))
+        //    {
+        //        ProcessDirectory(path); // nếu file tồn tại
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("{0} : Không đọc được file.", path);
+        //    }
+        //}
 
-        public void ProcessFile(string path, string strfileName)
-        {
-            //MessageBox.Show(""+ path + "\n" + strfileName);
-            //System.IO.File.Move((string)oldFileName, (string)newFileName);                      
-        }
+        //public void ProcessDirectory(string pathfile)
+        //{
+        //    string[] fileList = Directory.GetFiles(pathfile);//lay danh sách file cho vao mảng
+        //    string strFileName = "";
+        //    //duyet mang file trong thư mục
 
-        private void btnChoiseFolder_Click(object sender, EventArgs e)// nút click vào sẽ hiển thị chọn folder
-        {
-           
-        }
+        //    foreach (string fileName in fileList)
+        //    {
+        //        strFileName = "";
+        //        strFileName = Path.GetFileName(fileName).Trim();
+        //        ProcessFile(fileName, strFileName);
+        //    }
+
+        //    string[] directorylist = Directory.GetDirectories(pathfile);//lấy danh sách target file cho vào mảng
+
+        //    //duyệt mảng target
+        //    foreach (string directory in directorylist)
+        //    {
+        //        ProcessDirectory(directory);
+        //    }
+        //}
+
+        //public void ProcessFile(string path, string strfileName)
+        //{
+        //    MessageBox.Show(""+ path + "\n" + strfileName);
+        //    //System.IO.File.Move((string)oldFileName, (string)newFileName);                      
+        //}
+
+        //private void btnChoiseFolder_Click(object sender, EventArgs e)// nút click vào sẽ hiển thị chọn folder
+        //{
+
+        //}
     }
 }
--- QL_BanDayNit/DoiTenFile.designer.cs
+++ QL_BanDayNit/DoiTenFile.designer.cs
@@ -29,17 +29,21 @@
         private void InitializeComponent()
         {
             this.btnOk = new System.Windows.Forms.Button();
-            this.txtTenFile = new System.Windows.Forms.TextBox();
+            this.txtFileName = new System.Windows.Forms.TextBox();
             this.txtFirstNumber = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.txtDirectory = new System.Windows.Forms.TextBox();
+            this.txtTotalFile = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnDoiTen = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnOk
             // 
             this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnOk.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOk.Location = new System.Drawing.Point(94, 124);
+            this.btnOk.Location = new System.Drawing.Point(135, 77);
             this.btnOk.Name = "btnOk";
             this.btnOk.Size = new System.Drawing.Size(109, 30);
             this.btnOk.TabIndex = 0;
@@ -47,24 +51,24 @@
             this.btnOk.UseVisualStyleBackColor = true;
             this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
             // 
-            // txtTenFile
+            // txtFileName
             // 
-            this.txtTenFile.Location = new System.Drawing.Point(111, 17);
-            this.txtTenFile.Name = "txtTenFile";
-            this.txtTenFile.Size = new System.Drawing.Size(100, 26);
-            this.txtTenFile.TabIndex = 1;
+            this.txtFileName.Location = new System.Drawing.Point(112, 12);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(194, 26);
+            this.txtFileName.TabIndex = 1;
             // 
             // txtFirstNumber
             // 
-            this.txtFirstNumber.Location = new System.Drawing.Point(111, 50);
+            this.txtFirstNumber.Location = new System.Drawing.Point(112, 45);
             this.txtFirstNumber.Name = "txtFirstNumber";
-            this.txtFirstNumber.Size = new System.Drawing.Size(100, 26);
+            this.txtFirstNumber.Size = new System.Drawing.Size(194, 26);
             this.txtFirstNumber.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 19);
+            this.label1.Location = new System.Drawing.Point(48, 14);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(58, 19);
             this.label1.TabIndex = 2;
@@ -73,22 +77,63 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(60, 53);
+            this.label2.Location = new System.Drawing.Point(61, 48);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(48, 19);
             this.label2.TabIndex = 2;
             this.label2.Text = "Từ Số";
+            // 
+            // txtDirectory
+            // 
+            this.txtDirectory.Enabled = false;
+            this.txtDirectory.Location = new System.Drawing.Point(19, 113);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.Size = new System.Drawing.Size(339, 26);
+            this.txtDirectory.TabIndex = 1;
+            // 
+            // txtTotalFile
+            // 
+            this.txtTotalFile.Enabled = false;
+            this.txtTotalFile.Location = new System.Drawing.Point(155, 145);
+            this.txtTotalFile.Name = "txtTotalFile";
+            this.txtTotalFile.Size = new System.Drawing.Size(178, 26);
+            this.txtTotalFile.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tổng Số File";
+            // 
+            // btnDoiTen
+            // 
+            this.btnDoiTen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDoiTen.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDoiTen.Location = new System.Drawing.Point(135, 177);
+            this.btnDoiTen.Name = "btnDoiTen";
+            this.btnDoiTen.Size = new System.Drawing.Size(109, 30);
+            this.btnDoiTen.TabIndex = 0;
+            this.btnDoiTen.Text = "Đổi Tên Tất Cả";
+            this.btnDoiTen.UseVisualStyleBackColor = true;
+            this.btnDoiTen.Click += new System.EventHandler(this.btnDoiTen_Click);
             // 
             // frmDoiTenFile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightCyan;
-            this.ClientSize = new System.Drawing.Size(292, 178);
+            this.ClientSize = new System.Drawing.Size(375, 212);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTotalFile);
+            this.Controls.Add(this.txtDirectory);
             this.Controls.Add(this.txtFirstNumber);
-            this.Controls.Add(this.txtTenFile);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.btnDoiTen);
             this.Controls.Add(this.btnOk);
             this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -107,9 +152,13 @@
         #endregion
 
         private System.Windows.Forms.Button btnOk;
-        private System.Windows.Forms.TextBox txtTenFile;
+        private System.Windows.Forms.TextBox txtFileName;
         private System.Windows.Forms.TextBox txtFirstNumber;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDirectory;
+        private System.Windows.Forms.TextBox txtTotalFile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnDoiTen;
     }
 }
