bc21dead6c018dcb07e3ec5dd0ed51df37bf1cca
vipnat <tuanit198x@gmail.com>
2018-10-26 23:56:18 +0700

    Add File Backup DB

---
+++ QL_BanDayNit/frmTrangChu.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace QL_BanDayNit
+{
+    partial class frmTrangChu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTrangChu));
+            this.btnThemKH = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnThemSP = new System.Windows.Forms.Button();
+            this.btnBanHang = new System.Windows.Forms.Button();
+            this.btnThemNhanVien = new System.Windows.Forms.Button();
+            this.btnDSHD = new System.Windows.Forms.Button();
+            this.btnDoanhThu = new System.Windows.Forms.Button();
+            this.bntNhapHang = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnThemKH
+            // 
+            this.btnThemKH.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThemKH.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnThemKH.Location = new System.Drawing.Point(12, 49);
+            this.btnThemKH.Name = "btnThemKH";
+            this.btnThemKH.Size = new System.Drawing.Size(132, 32);
+            this.btnThemKH.TabIndex = 4;
+            this.btnThemKH.Text = "Khách Hàng";
+            this.btnThemKH.UseVisualStyleBackColor = true;
+            this.btnThemKH.Click += new System.EventHandler(this.btnThemKH_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Image = ((System.Drawing.Image)(resources.GetObject("btnThoat.Image")));
+            this.btnThoat.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnThoat.Location = new System.Drawing.Point(363, 181);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(77, 31);
+            this.btnThoat.TabIndex = 5;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(92, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 26);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Quản Lý Bán Hàng";
+            // 
+            // btnThemSP
+            // 
+            this.btnThemSP.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThemSP.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnThemSP.Location = new System.Drawing.Point(12, 137);
+            this.btnThemSP.Name = "btnThemSP";
+            this.btnThemSP.Size = new System.Drawing.Size(132, 32);
+            this.btnThemSP.TabIndex = 4;
+            this.btnThemSP.Text = "Sản Phẩm";
+            this.btnThemSP.UseVisualStyleBackColor = true;
+            this.btnThemSP.Click += new System.EventHandler(this.btnThemSP_Click);
+            // 
+            // btnBanHang
+            // 
+            this.btnBanHang.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBanHang.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnBanHang.Location = new System.Drawing.Point(182, 49);
+            this.btnBanHang.Name = "btnBanHang";
+            this.btnBanHang.Size = new System.Drawing.Size(132, 32);
+            this.btnBanHang.TabIndex = 4;
+            this.btnBanHang.Text = "Bán Hàng";
+            this.btnBanHang.UseVisualStyleBackColor = true;
+            this.btnBanHang.Click += new System.EventHandler(this.btnBanHang_Click);
+            // 
+            // btnThemNhanVien
+            // 
+            this.btnThemNhanVien.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThemNhanVien.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnThemNhanVien.Location = new System.Drawing.Point(12, 93);
+            this.btnThemNhanVien.Name = "btnThemNhanVien";
+            this.btnThemNhanVien.Size = new System.Drawing.Size(132, 32);
+            this.btnThemNhanVien.TabIndex = 4;
+            this.btnThemNhanVien.Text = "Nhân Viên";
+            this.btnThemNhanVien.UseVisualStyleBackColor = true;
+            this.btnThemNhanVien.Click += new System.EventHandler(this.btnThemNhanVien_Click);
+            // 
+            // btnDSHD
+            // 
+            this.btnDSHD.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDSHD.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnDSHD.Location = new System.Drawing.Point(182, 93);
+            this.btnDSHD.Name = "btnDSHD";
+            this.btnDSHD.Size = new System.Drawing.Size(132, 32);
+            this.btnDSHD.TabIndex = 4;
+            this.btnDSHD.Text = "List Hóa Đơn";
+            this.btnDSHD.UseVisualStyleBackColor = true;
+            this.btnDSHD.Click += new System.EventHandler(this.btnDSHD_Click);
+            // 
+            // btnDoanhThu
+            // 
+            this.btnDoanhThu.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDoanhThu.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnDoanhThu.Location = new System.Drawing.Point(182, 137);
+            this.btnDoanhThu.Name = "btnDoanhThu";
+            this.btnDoanhThu.Size = new System.Drawing.Size(132, 32);
+            this.btnDoanhThu.TabIndex = 4;
+            this.btnDoanhThu.Text = "Doanh Thu";
+            this.btnDoanhThu.UseVisualStyleBackColor = true;
+            this.btnDoanhThu.Click += new System.EventHandler(this.btnDoanhThu_Click);
+            // 
+            // bntNhapHang
+            // 
+            this.bntNhapHang.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntNhapHang.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.bntNhapHang.Location = new System.Drawing.Point(12, 181);
+            this.bntNhapHang.Name = "bntNhapHang";
+            this.bntNhapHang.Size = new System.Drawing.Size(132, 32);
+            this.bntNhapHang.TabIndex = 4;
+            this.bntNhapHang.Text = "Nhập Hàng";
+            this.bntNhapHang.UseVisualStyleBackColor = true;
+            this.bntNhapHang.Click += new System.EventHandler(this.bntNhapHang_Click);
+            // 
+            // frmTrangChu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(452, 224);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnThemNhanVien);
+            this.Controls.Add(this.bntNhapHang);
+            this.Controls.Add(this.btnDoanhThu);
+            this.Controls.Add(this.btnDSHD);
+            this.Controls.Add(this.btnBanHang);
+            this.Controls.Add(this.btnThemSP);
+            this.Controls.Add(this.btnThemKH);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "frmTrangChu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Thông báo doanh thu";
+            this.Load += new System.EventHandler(this.frmHoTroThongBaoDoanhThu_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnThemKH;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnThemSP;
+        private System.Windows.Forms.Button btnBanHang;
+        private System.Windows.Forms.Button btnThemNhanVien;
+        private System.Windows.Forms.Button btnDSHD;
+        private System.Windows.Forms.Button btnDoanhThu;
+        private System.Windows.Forms.Button bntNhapHang;
+    }
+}
